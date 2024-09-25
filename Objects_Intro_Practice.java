cb7ece643be27753b1ae779d4ae457bf17bc9a54
mimsterrss <mhall2025@damien.edu>
2024-09-25 09:43:47 -1000

    Update Objects_Intro_Practice.java

--- Objects_Intro_Practice.java
+++ Objects_Intro_Practice.java
@@ -1,1 +1,51 @@
+/* Today is Wednesday, September 25th. My name is Mimi Hall and this class is used to help us learn 
+* how to make a Circle class withOUT using the main method. It declares the data attributes, functionality, and the formulas
+* for each forumla of a circle. This includes the radius, diameter, area, and circumference. Going back into the main method,
+* you are able to print out the methods without having to type out the formula within the main method. 
+*/
 
+public class Circle {
+	//data attributes
+	private final double pi = 3.14;
+	private double radius;
+	
+	//constructor
+	public Circle (double r)
+	{
+		radius = r;
+		
+	}
+	
+	//functionality
+	
+	//declare radius
+	public double getRadius()
+	{
+		return radius;
+	}
+	
+	//formula for diameter
+	public double getDiameter()
+	{
+		return radius+radius;
+	}
+	
+	//formula for circumference
+	public double getCircumference()
+	{
+		return 2*pi*radius;
+	}
+	
+	//formula for area
+	public double getArea()
+	{
+		return pi*(radius*radius);
+	}
+	
+	//declare the name of the class
+	public static String getShape()
+	{
+		return "Circle";
+	}
+
+}
