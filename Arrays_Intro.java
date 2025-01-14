72f18562019de7dd780734443ca0fa744bf28c20
mimsterrss <mhall2025@damien.edu>
2025-01-13 14:40:01 -1000

    Create Arrays_Intro.java

---
+++ Arrays_Intro.java
@@ -0,0 +1,31 @@
+//question one 
+// create empty arrays that represent doubles, ints, booleans, string objects, and circle objects
+double[] doubArr;
+
+int[] intArr;
+
+boolean[] boolArr;
+
+string[] strArr;
+
+//figure this one out!!!!!
+double[] circArr;
+
+
+
+//question two
+//create an array of ints with the following represented values: 7, -12, 88, -88, 0, 5, 2000
+int[] intArr = {7, -12, 88, -88, 0, 5, 2000};
+
+
+
+//question three
+//create an array of 5 strings objects with default values.
+//state the default value for ints, booleans, doubles, and any reference data type as well !!!!!!!!
+string[] fruitsArr = {"Apple", "Banana", "Kiwi", "Mango", "Watermelon"};
+
+
+
+//question four
+//change the second element in fruitsArr to "Bapple"
+
