c52b0bbb803e7ddc0f534a28e0a4d5461fa7600e
mimsterrss <mhall2025@damien.edu>
2025-01-17 13:17:38 -1000

    Update Array_One.java

--- CodingBatSolns/Array_One.java
+++ CodingBatSolns/Array_One.java
@@ -10,14 +10,61 @@ public boolean firstLast6(int[] nums) {
   
 }
 
-//sameFirstLast!!!!
+
+
+//sameFirstLast
 public boolean sameFirstLast(int[] nums) {
   
-  if(nums[nums.length-1]>nums.length && nums[0]==nums[nums.length-1]){
+  if(nums.length==0)
+  {
+    return false;
+  }
+  else if(nums[0]==nums[nums.length-1])
+  {
     return true;
   }
-  else{
+  else
+  {
+    return false;
+  }
+
+}
+
+
+
+//makePi
+public int[] makePi() {
+  
+  int[] piArr = {3, 1, 4};
+  return piArr;
+  
+}
+
+
+
+//commonEnd
+public boolean commonEnd(int[] a, int[] b) {
+  
+  if(a[0]==b[0] || a[a.length-1]==b[b.length-1])
+  {
+    return true;
+  }
+  else
+  {
     return false;
   }
   
 }
+
+
+
+//sum3
+public int sum3(int[] nums) {
+  
+  return nums[0]+nums[1]+nums[2];
+
+}
+
+
+
+
