5075d239c365f974b223bb7ab1285c4f0f847b15
mimsterrss <mhall2025@damien.edu>
2025-01-22 12:50:25 -1000

    Update Array_Two.java

--- CodingBatSolns/Array_Two.java
+++ CodingBatSolns/Array_Two.java
@@ -81,3 +81,21 @@ public int sum2(int[] nums) {
 //middleWay
 //Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
 
+public int[] middleWay(int[] a, int[] b) {
+  
+  int[] newArr = {a[1], b[1]};
+  return newArr;
+  
+}
+
+
+
+//makeEnds
+//Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+
+public int[] makeEnds(int[] nums) {
+  
+  int[] newArr = {nums[0], nums[nums.length-1]};
+  return newArr;
+  
+}
