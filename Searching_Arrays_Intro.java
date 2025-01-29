c0b1e9a900c6ffbc95b35030ac86a57b8e6c0c81
mimsterrss <mhall2025@damien.edu>
2025-01-29 12:55:26 -1000

    Update Searching_Arrays_Intro.java

--- Searching_Arrays_Intro.java
+++ Searching_Arrays_Intro.java
@@ -1,11 +1,11 @@
 //question one
 //Create a method checkFor4() that is passed an int array and returns true if that array has a 4 and false if that array has no 4's. 
 
-public static int checkfor4(int[] arr, int m)
+public static boolean checkfor4(int[] arr, int m)
 {
 	int[] copyArr = arr;
 	boolean isFound = false;
-	for(int i = 0; I < arr.length; i++)
+	for(int i = 0; i < arr.length; i++)
 	{
 		if(m == copyArr[i])
 		{
