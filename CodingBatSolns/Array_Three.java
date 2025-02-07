e45e8d37cf3e2a83538c2e2daa1cc7a5bf15239b
mimsterrss <mhall2025@damien.edu>
2025-02-07 10:02:43 -1000

    Update Array_Three.java

--- CodingBatSolns/Array_Three.java
+++ CodingBatSolns/Array_Three.java
@@ -231,3 +231,35 @@ public int[] frontPiece(int[] nums) {
 //We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
 
 
+
+
+
+//make2
+//
+
+
+
+
+
+//front11
+//Given 2 int arrays, a and b, of any length, return a new array with the first element of each array. If either array is length 0, ignore that array.
+
+public int[] front11(int[] a, int[] b) {
+  
+  if(a.length > 0 && b.length > 0) 
+  {
+      return new int[] {a[0], b[0]};
+    } 
+    else if(a.length > 0) 
+    {
+        return new int[] {a[0]};
+    } 
+    else if(b.length > 0)
+    {
+      return new int[] {b[0]};
+    }
+    return new int[0];
+  
+}
+
+
