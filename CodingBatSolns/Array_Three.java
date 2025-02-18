3d7605ade7da734b965329ac83468968c6af890c
mimsterrss <mhall2025@damien.edu>
2025-02-18 08:32:53 -1000

    Update Array_Three.java

--- CodingBatSolns/Array_Three.java
+++ CodingBatSolns/Array_Three.java
@@ -249,7 +249,38 @@ public boolean unlucky1(int[] nums) {
 //make2
 //Given 2 int arrays, a and b, return a new array length 2 containing, as much as will fit, the elements from a followed by the elements from b. The arrays may be any length, including 0, but there will be 2 or more elements available between the 2 arrays.
 
-
+public int[] make2(int[] a, int[] b) {
+  
+  int[] copyArr = new int[2];
+    int tempA = a.length;
+    int tempB = b.length;
+  
+  if (tempA == 0) 
+  {
+    copyArr[0] = b[0];
+    copyArr[1] = b[1];
+    return copyArr;
+  } 
+  else if (tempA == 1 && tempB == 1) 
+  {
+    copyArr[0] = a[0];
+    copyArr[1] = b[0];
+    return copyArr;
+  } 
+  else if (tempA == 1) 
+  {
+      copyArr[0] = a[0];
+      copyArr[1] = b[0];
+      return copyArr;  
+  } 
+  else 
+  {
+      copyArr[0] = a[0];
+      copyArr[1] = a[1];
+      return copyArr;
+  }
+  
+}
 
 
 
