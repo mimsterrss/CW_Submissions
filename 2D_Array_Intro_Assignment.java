83a6b489e82e1676f39e937a59e53020ebbdce86
mimsterrss <mhall2025@damien.edu>
2025-03-28 13:07:06 -1000

    Create 2D_Array_Intro_Assignment.java

---
+++ 2D_Array_Intro_Assignment.java
@@ -0,0 +1,24 @@
+//task one: Instantiate a 2-D array gridNums that consists of 4 rows and 6 columns using the new keyword. 
+
+//task two: Instantiate a 2-D array with the following values:
+
+
+//For the following problems, print out those elements before and after to verify it worked. 
+
+//task three: For both of those arrays, change the element in the first row, first column to a 17.
+
+//task four: For both of those arrays, change the element in the second row, third column into a 20. 
+
+//task five: For both of those arrays, change the last element in the first row to a -5. Accomplish this using .length 
+
+//task six: For both of those arrays, change the first element in the last row to a -7. Accomplish this using .length 
+
+//task seven: For both of those arrays, change the last element in the last row to a -30. Accomplish this using .length 
+
+//task eight: Create a method that is passed a 2-D array of ints and one by one prints out each element in the first row. 
+
+//task nine: Create a method that is passed a 2-D array of ints and one by one prints out each element in the first column.
+
+//task ten: Create a method that is passed a 2-D array of ints and one by one prints out each element in row-major order. 
+
+//task eleven: Create a method that is passed a 2-D array of ints and one by one prints out each element in column-major order. 
