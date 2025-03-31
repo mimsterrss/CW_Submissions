19e5da55493bfeb288a000c83a2d99cc1e05cc4b
mimsterrss <mhall2025@damien.edu>
2025-03-31 12:51:14 -1000

    Update 2D_Array_Intro_Assignment.java

--- 2D_Array_Intro_Assignment.java
+++ 2D_Array_Intro_Assignment.java
@@ -4,7 +4,9 @@ int[][] gridNums = new int[4][6];
 
 //task two: Instantiate a 2-D array with the following values:
 
-int[][] gridNums = {4, 5, 7}, {2, -1, 8}, {0, 1, 3};
+int[][] gridNums = {4, 5, 7}, 
+                   {2, -1, 8}, 
+                   {0, 1, 3};
 
 //For the following problems, print out those elements before and after to verify it worked. 
 //task three: For both of those arrays, change the element in the first row, first column to a 17.
