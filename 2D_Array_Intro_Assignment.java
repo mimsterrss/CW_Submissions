dbf543b790482de1a7ef924da568464e36d233e9
mimsterrss <mhall2025@damien.edu>
2025-04-11 14:14:02 -1000

    Update 2D_Array_Intro_Assignment.java

--- 2D_Array_Intro_Assignment.java
+++ 2D_Array_Intro_Assignment.java
@@ -1,27 +1,45 @@
 //task one: Instantiate a 2-D array gridNums that consists of 4 rows and 6 columns using the new keyword. 
 
-int[][] gridNums = new int[4][6];
+int[][] gridNumsOne = new int[4][6];
+
+
 
 //task two: Instantiate a 2-D array with the following values:
 
-int[][] gridNums = {4, 5, 7}, 
-                   {2, -1, 8}, 
-                   {0, 1, 3};
+int[][] gridNumsTwo = {4, 5, 7}, 
+                      {2, -1, 8}, 
+                      {0, 1, 3};
+
+
 
 //For the following problems, print out those elements before and after to verify it worked. 
 //task three: For both of those arrays, change the element in the first row, first column to a 17.
 
-gridnums[0][0] = [17];
+gridnumsOne[0][0] = [17];
+gridnumsTwo[0][0] = [17];
+
+
 
 //task four: For both of those arrays, change the element in the second row, third column into a 20. 
 
-gridNums[1][2] = [20];
+gridNumsOne[1][2] = [20];
+gridNumsTwo[1][2] = [20];
+
+
 
 //task five: For both of those arrays, change the last element in the first row to a -5. Accomplish this using .length 
 
-gridNums[0][gridNums[0].length-1] = [-5];
+gridNumsOne[0][gridNumsOne[0].length-1] = [-5];
+gridNumsTwo[0][gridNumsTwo[0].length-1] = [-5];
+
+
 
 //task six: For both of those arrays, change the first element in the last row to a -7. Accomplish this using .length 
+
+gridNumsOne[3][gridNums[3].length-4] = -7;
+gridNumsTwo[0][
+
+
 
 //task seven: For both of those arrays, change the last element in the last row to a -30. Accomplish this using .length 
 
