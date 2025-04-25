520aa84f69ed31ce96b0854aa3f0af88ae552687
mimsterrss <mhall2025@damien.edu>
2025-04-25 08:43:24 -1000

    Update 2D_Array_Intro_Assignment.java

--- 2D_Array_Intro_Assignment.java
+++ 2D_Array_Intro_Assignment.java
@@ -36,17 +36,96 @@ gridNumsTwo[0][gridNumsTwo[0].length-1] = [-5];
 
 //task six: For both of those arrays, change the first element in the last row to a -7. Accomplish this using .length 
 
-gridNumsOne[3][gridNums[3].length-4] = -7;
-gridNumsTwo[0][
+gridNumsOne[3][gridNumsOne[0].length-3] = -7;
+gridNumsTwo[2][gridNumsTwo[0].length-1] = -7;
 
 
 
 //task seven: For both of those arrays, change the last element in the last row to a -30. Accomplish this using .length 
 
+gridNumsOne[3][gridNumsOne[0].length-1] = -30;
+gridNumsTwo[2][gridNumsTwo[0].length-1] = -30;
+
+  
+  
 //task eight: Create a method that is passed a 2-D array of ints and one by one prints out each element in the first row. 
+
+	public static int[][] elementsInFirstRow(int[][] arr){
+    
+		//loop to go through the 2-D array
+		for(int i=0; i<=arr[0].length-1; i++){
+      
+			//will print whatever element 'i' is currently on in the first row of the array
+			System.out.println(arr[0][i]);
+      
+		}
+		//return the array
+		return arr;
+    
+	}
+
+
 
 //task nine: Create a method that is passed a 2-D array of ints and one by one prints out each element in the first column.
 
+	public static int[][] elementsInFirstColumn(int[][] arr){
+    
+		//loop to go through the 2-D array
+		for(int i=0; i<=arr.length-1; i++){
+      
+			//prints whatever element 'i' is currently on in the first column of the array
+			System.out.print(arr[i][0]);
+      
+		}
+		//return the array
+		return arr;
+		
+	}
+
+
+
 //task ten: Create a method that is passed a 2-D array of ints and one by one prints out each element in row-major order. 
 
+	public static int[][] elementsInRowMajorOrder(int[][] arr){
+		
+    //loop to go through the 2-D array
+		for (int i=0; i<=arr[0].length-1; i++){
+			
+			//this for loop is for the rows & will continue to run as along as 'r' is less than or equal to arr.length - 1
+			for(int r=0; r<=arr.length-1; r++){
+        
+				//prints whatever 'i' and 'r' are equal to
+				System.out.println(arr[r][i]);
+        
+			}
+      
+		}
+		//returns the array
+		return arr;
+    
+	}
+	
+
+
 //task eleven: Create a method that is passed a 2-D array of ints and one by one prints out each element in column-major order. 
+
+	public static int[][] elementsInColumnMajorOrder(int[][] arr){
+    
+		//loop to go through the 2-D array
+		for(int i=0; i<=arr.length-1;i++){
+      
+			////this for loop is for the columns & will continue to run as along as 'r' is less than arr.length - 1
+			for(int r=0;r<arr[0].length-1;r++){
+        
+				//prints whatever 'i' and 'r' are equal to
+				System.out.println(arr[i][r]);
+        
+			}
+      
+		}
+		//returns the array
+		return arr;
+    
+	}
+
+
