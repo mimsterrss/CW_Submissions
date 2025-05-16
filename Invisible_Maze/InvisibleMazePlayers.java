355a2476c674871e5008024eab4e3c6d5fa8c112
mimsterrss <mhall2025@damien.edu>
2025-05-16 11:00:51 -1000

    Update InvisibleMazePlayers.java

--- Invisible_Maze/InvisibleMazePlayers.java
+++ Invisible_Maze/InvisibleMazePlayers.java
@@ -1,1 +1,68 @@
 
+
+public class InvisibleMazePlayers {
+
+	int rPos;
+	int cPos;
+	
+	public InvisibleMazePlayers() {
+		
+		//sets both rPos & cPos (attributes) to zero
+		rPos = 0;
+		cPos = 0;
+		
+	}
+	
+	public int[] getPos() {
+		
+		//puts the rPos & cPos together
+		int[] pos = {rPos, cPos};
+		
+		//returns the pos
+		return pos;
+		
+	}
+	
+	//gets the rows
+	public int getrPos() {
+		
+		return rPos;
+	
+	}
+
+	//sets the rPos
+	public void setrPos(int rPos) {
+	
+		this.rPos = rPos;
+		
+	}
+
+	//gets the columns
+	public int getcPos() {
+		
+		return cPos;
+		
+	}
+
+	//sets the cPos
+	public void setcPos(int cPos) {
+		
+		this.cPos = cPos;
+		
+	}
+	
+	//sets the columns
+	public void setC(int newC){
+		
+		cPos = newC;
+		
+	}
+	
+	//sets the rows
+	public void setR(int newR){
+		
+		rPos = newR;
+	}
+	
+}
+
